e778f466700e6e6c4e89644b847f930fc7a50223
joelsnz <joel04slm@gmail.com>
2025-11-17 08:23:29 +0100

    terminal listarray

--- List.h
+++ List.h
@@ -3,17 +3,17 @@
 
 #include <stdexcept>
 
-template <typename T>
-class List {
-	virtual ~List() {}
-	virtual void insert(int pos, T e) = 0;
-	virtual void append(T e) = 0;
-	virtual void prepend(T e) = 0;
-	virtual T remove(int pos) = 0;
-	virtual T get(int pos) const = 0;
-	virtual int search(T e) const = 0;
-	virtual bool empty() const = 0;
-	virtual int size() const = 0;
+template <typename T> class List {
+public:
+  virtual ~List() {}
+  virtual void insert(int pos, T e) = 0;
+  virtual void append(T e) = 0;
+  virtual void prepend(T e) = 0;
+  virtual T remove(int pos) = 0;
+  virtual T get(int pos) const = 0;
+  virtual int search(T e) const = 0;
+  virtual bool empty() const = 0;
+  virtual int size() const = 0;
 };
 
 #endif // LIST_H
--- ListArray.h
+++ ListArray.h
@@ -13,37 +13,40 @@ private:
   static const int MINSIZE = 2;
 
 public:
-  ListArray() { this->arr = malloc(sizeof(T) * MINSIZE); }
+  ListArray() : arr(new T[MINSIZE]), max(MINSIZE), n(0) {}
 
-  ~ListArray() { delete[](this->arr); }
+  ~ListArray() { delete[] arr; }
 
-  T operator[](int pos) {
-    if(pos >= 0 || pos < this->n) return this->arr[this->n];
+  T operator[](int pos) const {
+    if(pos >= 0 && pos < this->n) return this->arr[pos];
     else throw std::out_of_range("Indice fuera de rango\n");
   }
 
   friend std::ostream &operator<<(std::ostream &out,
                                   const ListArray<T> &list) {
     out << "[";
-    for(int i = 0; i < list.n - 1; i++)
-      out << list[i] << ", ";
-    out << list[list.n] << "]";
+    for(int i = 0; i < list.n; i++) {
+      if(i > 0) out << ", ";
+      out << list[i];
+    }
+    out << "]";
     return out;
   }
 
   void resize(int new_size) {
-    T arr = new T[new_size];
-    int data_size = new_size < this->max ? new_size : this->n;
-    for(int i = 0; i < data_size; i++)
-      arr[i] = this->arr[i];
+    T *new_arr = new T[new_size];
+    int limit = n < new_size ? n : new_size;
+    for(int i = 0; i < limit; i++)
+      new_arr[i] = this->arr[i];
     delete[] this->arr;
-    this->arr = arr;
+    this->arr = new_arr;
     this->max = new_size;
+    if(n > new_size) n = new_size;
   }
 
   virtual void insert(int pos, T e) {
     if(pos < 0 || pos > n)
-      throw std::out_of_range("Posicion invalida en insert()");
+      throw std::out_of_range("Posicion invalida");
     if(n == max) resize(max * 2);
     for(int i = n; i > pos; i--)
       arr[i] = arr[i - 1];
@@ -56,12 +59,21 @@ public:
   virtual void prepend(T e) { insert(0, e); }
 
   virtual T remove(int pos) {
-    for(int i = pos; i < this->n; i++)
-      this->arr[pos] = this->arr[pos - 1];
+    if(pos < 0 || pos >= n)
+      throw std::out_of_range("Posicion invalida");
+    T removed = this->arr[pos];
+    for(int i = pos; i < this->n - 1; i++) {
+      this->arr[pos] = this->arr[pos + 1];
+    }
     n--;
+    return removed;
   }
 
-  virtual T get(int pos) const { return this->arr[pos]; }
+  virtual T get(int pos) const {
+    if(pos < 0 || pos >= n)
+      throw std::out_of_range("Posicion invalida");
+    return this->arr[pos];
+  }
 
   virtual int search(T e) const {
     for(int i = 0; i < this->n; i++) {
