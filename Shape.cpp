6c1fffa7901dd1d10487b6dbd4cd7bfbdb22fdb6
joelsnz <joel04slm@gmail.com>
2025-11-20 09:01:45 +0100

    shape

---
+++ Shape.cpp
@@ -0,0 +1,20 @@
+#include "Shape.h"
+
+Shape::Shape() : color("red") {}
+
+Shape::Shape(std::string color) {
+  std::string valid_colors[] = {"red", "green", "blue"};
+  bool is_valid = false;
+  for(std::string c : valid_colors) {
+    if(color == c) {
+      is_valid = true;
+      break;
+    } 
+  }
+
+  if(!is_valid) throw std::invalid_argument("color invalido");
+
+  this->color = color;
+}
+
+std::string Shape::get_color() const { return this->color; }
