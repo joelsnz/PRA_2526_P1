3d65893d1f1fd49fbf080e7b096000b9be6e0dd9
JSANM14J <jsanm14j@alumno.upv.es@ldsic-vdi01.upvnet.upv.es>
2025-11-20 08:20:37 +0100

    declaracion point2d

--- Point2D.h
+++ Point2D.h
@@ -6,11 +6,11 @@
 class Point2D {
 public:
   double x, y;
-  Point2D(double x=0, double y=0) x(x), y(y) {}
+  Point2D(double x=0, double y=0) : x(x), y(y) {}
   static double distance(const Point2D& a, const Point2D& b);
-  bool operator==(const Point2D& a, const Point2D& b);
-  bool operator!=(const Point2D& a, const Point2D& b);
-  std::ostream& operator<<(std::ostream& out, const Point2D& p);
+  friend bool operator==(const Point2D& a, const Point2D& b);
+  friend bool operator!=(const Point2D& a, const Point2D& b);
+  friend std::ostream& operator<<(std::ostream& out, const Point2D& p);
 };
 
 #endif // POINT2D_H
